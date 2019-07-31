7c59cb2d38fadd3374fdc520e1b28bd6970f65df
Cosmin-Gabriel Samoila <gabrielcsmo@gmail.com>
2019-07-31 22:19:01 +0300

    Add minimum parsing functions for an working input
    Signed-off-by: Cosmin-Gabriel Samoila <gabrielcsmo@gmail.com>

--- lammps/input_file.py
+++ lammps/input_file.py
@@ -35,35 +35,88 @@ class LammpsInputFile():
 
         self.masses = []
         self.atoms = []
+        self.velocities = []
+        self.bonds = []
+        self.angles = []
 
         """parse the file"""
         self.read_file()
 
     def parse_nonbond_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_bond_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_angle_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_dihedral_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_improper_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_bondbond_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_bondangle_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_middlebondtorsion_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_endbondtorsion_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_angletorsion_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_angleangletorsion_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_bondbond13_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
+
     def parse_angleangle_coeffs(self):
+        self.current_index = len(self.lines)
+        print("Not implemented!")
+        pass
+
+    def parse_dihedrals(self):
+        print("Parsing Dihedrals starting at line #{}".format(self.current_index))
+        self.current_index = len(self.lines)
+        print("Not implemented!")
+        pass
+
+    def parse_impropers(self):
+        print("Parsing Impropers starting at line #{}".format(self.current_index))
+        self.current_index = len(self.lines)
+        print("Not implemented!")
         pass
 
     def parse_masses(self):
@@ -105,21 +158,50 @@ class LammpsInputFile():
         self.current_index = end
 
     def parse_velocities(self):
-        print("Parsing Velocities starting at line #{}".format(self.current_index))
-        self.current_index = len(self.lines)
-        pass
+        N = self.header["atoms"]
+        print("Parsing {} Velocities starting at line #{}".format(N, self.current_index))
+        start = self.current_index + 1
+        end = start + N
+        for l in self.lines[start:end]:
+            items = l.split()
+            idx = int(items[0])
+            vx = float(items[1])
+            vy = float(items[2])
+            vz = float(items[3])
+            vel = (idx, vx, vy, vz)
+            self.velocities.append(vel)
+        self.current_index = end
+
     def parse_bonds(self):
-        print("Parsing Bonds starting at line #{}".format(self.current_index))
-        pass
+        N = self.header["bonds"]
+        print("Parsing {} Bonds starting at line #{}".format(N, self.current_index))
+        start = self.current_index + 1
+        end = start + N
+        for l in self.lines[start:end]:
+            items = l.split()
+            idx = int(items[0])
+            bond_type = int(items[1])
+            atom1 = int(items[2])
+            atom2 = int(items[3])
+            bond = (idx, bond_type, atom1, atom2)
+            self.bonds.append(bond)
+        self.current_index = end
+
     def parse_angles(self):
-        print("Parsing Angles starting at line #{}".format(self.current_index))
-        pass
-    def parse_dihedrals(self):
-        print("Parsing Dihedrals starting at line #{}".format(self.current_index))
-        pass
-    def parse_impropers(self):
-        print("Parsing Impropers starting at line #{}".format(self.current_index))
-        pass
+        N = self.header["angles"]
+        print("Parsing {} Angles starting at line #{}".format(N, self.current_index))
+        start = self.current_index + 1
+        end = start + N
+        for l in self.lines[start:end]:
+            items = l.split()
+            idx = int(items[0])
+            angle_type = int(items[1])
+            atom1 = int(items[2])
+            atom2 = int(items[3])
+            atom3 = int(items[4])
+            angle = (idx, angle_type, atom1, atom2, atom3)
+            self.angles.append(angle)
+        self.current_index = end
 
     def parse_header(self):
         for i in range(len(self.lines)):
@@ -194,13 +276,19 @@ class LammpsInputFile():
         self.input_file.close()
         print("All cleared!")
 
+    def __str__(self):
+        res = "Header: " + str(self.header) + "\n"
+        res += "Box: " + str(self.box) + "\n"
+        res += "Masses: " + str(self.masses) + "\n"
+        res += "Atoms: [" + str(self.atoms[0]) + "\n\t\t. . .\n\t" + str(self.atoms[-1]) + "]\n"
+        res += "Velocities: [" + str(self.velocities[0]) + "\n\t\t. . .\n\t" + str(self.velocities[-1]) + "]\n"
+        res += "Bonds: [" + str(self.bonds[0]) + "\n\t\t. . .\n\t" + str(self.bonds[-1]) + "]\n"
+        res += "Angles: [" + str(self.angles[0]) + "\n\t\t. . .\n\t" + str(self.angles[-1]) + "]\n"
+        return res
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: {} <input_file>".format(sys.argv[0]))
         sys.exit(-1)
     l = LammpsInputFile(sys.argv[1])
-    print(l.header)
-    print(l.masses)
-    #print(l.atoms)
-
+    print(l)
